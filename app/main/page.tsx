e9a5ee52ff43e000cb0e502f43417e05114f47b1
Changwhi <whinada@gmail.com>
2024-06-27 22:00:34 -0700

    design: create layout and a button for a new interview

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,10 +1,18 @@
+import AddNewInterview from '@/components/main/AddNewInterview'
 import { UserButton } from '@clerk/nextjs'
 import React, { useEffect } from'react'
 
 const Main = () => {
 
   return (
-  <UserButton />
+    <div className='p-10'>
+      <h2 className='font-bold text-2xl'>Main</h2>
+      <h2 className='text-gray-500'>Start your Mockup Interview with AI!</h2>
+      <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
+        <AddNewInterview />
+      </div>
+    </div>
+
   )
 }
 
