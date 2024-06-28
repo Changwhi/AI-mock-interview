57a74f19636faec8fa0d4cfa6d1eb53521647b14
Changwhi <whinada@gmail.com>
2024-06-27 17:15:06 -0700

    feat: create schema for a mockInterview table

---
+++ utils/schema.ts
@@ -0,0 +1,15 @@
+import { integer, pgEnum, pgTable, serial, text, uniqueIndex, varchar } from 'drizzle-orm/pg-core';
+
+type UserId = number & { __brand: 'user_id' };
+export const popularityEnum = pgEnum('popularity', ['unknown', 'known', 'popular']);
+
+export const countries = pgTable('mockInterview', {
+  id: serial('id').primaryKey(),
+  jsonMockResp:text('jsonMockResp').notNull(),
+  jobPosition:varchar('jobPosition').notNull(),
+  jobDescription:varchar('jobDescription').notNull(),
+  jobExperience:varchar('jobExperience').notNull(),
+  createdBy:varchar('createdBy').notNull(),
+  createdAt:varchar('createdAt').notNull(),
+  mockId:varchar('mockId').notNull(),
+});
