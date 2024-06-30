dbc912a5338f9e743f71b3b3d02159435a4725c8
Changwhi <whinada@gmail.com>
2024-06-30 00:18:46 -0700

    refactor: create webcam component

---
+++ app/main/interview/[interviewId]/_component/WebcamComponent.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React, { useState } from 'react'
+import { Lightbulb, WebcamIcon } from "lucide-react";
+import { Button } from '@/components/ui/button';
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user",
+};
+function WebcamComponent() {
+  const [webCamEnabled, setWebCamEnabled] = useState(false);
+  return (
+    <div>
+            {webCamEnabled ? (
+              <>
+                <Webcam
+                  mirrored={true}
+                  videoConstraints={videoConstraints}
+                />
+                
+                <Button variant={"outline"} className="self-center" onClick={() => setWebCamEnabled(false)}>
+                  Disable Webcam
+                </Button>
+              </>
+            ) : (
+              <>
+                <div className="h-full w-full items-center flex flex-col justify-center text-center text-semibold border bg-secondary rounded-md">
+                  <WebcamIcon className="w-20 h-20" />
+                </div>
+                <Button variant={"outline"} className="self-center" onClick={() => setWebCamEnabled(true)}>
+                  Enable Webcam
+                </Button>
+              </>
+            )}
+
+
+    </div>
+  )
+}
+
+export default WebcamComponent
