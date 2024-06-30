1927dd7db220fe229f2c7cdc6e7eba9b0f5048f2
Changwhi <whinada@gmail.com>
2024-06-30 00:43:31 -0700

    fix: display skeleton when fetching data from custom hook

--- app/main/interview/[interviewId]/start/page.tsx
+++ app/main/interview/[interviewId]/start/page.tsx
@@ -4,15 +4,28 @@ import React, { useEffect, useState } from "react";
 import QuestionsSection from "./_components/QuestionsSection";
 import Record from "./_components/Record";
 import useGetInterviewDetails from "@/lib/useGetInterviewDetails";
+import { Skeleton } from "@/components/ui/skeleton";
 
 function Start({ params }: { params: { interviewId: string } }) {
   const [activeQuestion, setActiveQuestion] = useState(0);
+  const [questions, setQuestions] = useState<MockInterviewType[]>([]);
   const response = useGetInterviewDetails(params.interviewId);
 
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <QuestionsSection questions={JSON.parse(response?.jsonMockResp as string)} activeQuestion={activeQuestion} />
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
+        {response && (
+          <QuestionsSection
+            questions={JSON.parse(response?.jsonMockResp as string)}
+            activeQuestion={activeQuestion}
+          />
+        )}
+        {!response && (
+          <div className="flex flex-col space-y-3">
+            <Skeleton className="h-[350px] w-[250px] rounded-xl" />
+            <Skeleton className="h-[350px] w-[250px] rounded-xl" />
+          </div>
+        )}
         <Record />
       </div>
     </div>
