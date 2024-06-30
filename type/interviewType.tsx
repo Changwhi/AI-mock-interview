f431b1df49e7810c53e1af2bbb519fe290854ea7
Changwhi <whinada@gmail.com>
2024-06-29 18:27:15 -0700

    feat: create interface for MockInterview data type

---
+++ type/interviewType.tsx
@@ -0,0 +1,10 @@
+export interface MockInterviewType {
+    id: number;
+    jsonMockResp: string;
+    jobPosition: string;
+    jobDescription: string;
+    jobExperience: string;
+    createdBy: string;
+    createdAt: string;
+    mockId: string;
+}
