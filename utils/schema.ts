7d160a5883f5ebc060cbfcfc16fcb4b6a0c9b45e
Changwhi <whinada@gmail.com>
2024-07-01 01:01:46 -0700

    feat: create schema for user answers and related information

--- utils/schema.ts
+++ utils/schema.ts
@@ -13,3 +13,16 @@ export const MockInterview = pgTable('mockInterview', {
   createdAt:varchar('createdAt').notNull(),
   mockId:varchar('mockId').notNull(),
 });
+
+
+export const Answer = pgTable('answer', {
+  id: serial('id').primaryKey(),
+  mockIdRef: varchar('mockId').notNull(),
+  question: varchar('question').notNull(),
+  correctAns: varchar('correctAns').notNull(),
+  userAnswer: varchar('answer').notNull(),
+  feedback: varchar('feedback').notNull(),
+  userEmail:varchar('userEmail').notNull(),
+  rating: varchar('rating').notNull(),
+  createdAt:varchar('createdAt').notNull(),
+});
