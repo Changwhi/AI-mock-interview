24ffff629eff03d4383a777744bb9ad4bd910a33
Changwhi <whinada@gmail.com>
2024-07-01 01:26:41 -0700

    refactor: import WebcamComponent directly to reduce dependency on this  page

--- app/main/interview/[interviewId]/start/page.tsx
+++ app/main/interview/[interviewId]/start/page.tsx
@@ -6,6 +6,7 @@ import Record from "./_components/Record";
 import useGetInterviewDetails from "@/lib/useGetInterviewDetails";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
+import WebcamComponent from "@/components/WebcamComponent";
 
 function Start({ params }: { params: { interviewId: string } }) {
   const [activeQuestion, setActiveQuestion] = useState(0);
@@ -15,25 +16,35 @@ function Start({ params }: { params: { interviewId: string } }) {
   return (
     <div className="p-10 flex flex-col">
       <h2 className="font-bold text-2xl">You are in an interview session</h2>
-      <h2 className="text-gray-500">Choose a question and click on "Record"</h2>
+      <h2 className="text-gray-500">
+        Choose a question and click on Record button
+      </h2>
       <div className="flex flex-col justify-center items-center gap-10">
         <div className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-10">
           {response && (
-            <QuestionsSection
-              setActiveQuestion={setActiveQuestion}
-              questions={JSON.parse(response?.jsonMockResp as string)}
-              activeQuestion={activeQuestion}
-            />
+            <>
+              <QuestionsSection
+                setActiveQuestion={setActiveQuestion}
+                questions={JSON.parse(response?.jsonMockResp as string)}
+                activeQuestion={activeQuestion}
+              />
+              <div className="flex flex-col md:my-20 justify-center items-center rounded-lg">
+                <WebcamComponent interviewInfo={response} questions={JSON.parse(response?.jsonMockResp as string)} activeQuestion={activeQuestion} cameraButton={false} />
+              </div>
+            </>
           )}
           {!response && (
-            <div className="flex flex-col space-y-3">
-              <Skeleton className="h-[350px] w-[250px] rounded-xl" />
-              <Skeleton className="h-[350px] w-[250px] rounded-xl" />
-            </div>
+            <>
+              <div className="flex flex-col space-y-3">
+                <Skeleton className="h-[350px] w-[250px] rounded-xl" />
+                <Skeleton className="h-[350px] w-[250px] rounded-xl" />
+              </div>
+              <div className="flex flex-col md:my-20 justify-center items-center rounded-lg">
+                <WebcamComponent interviewInfo={null} questions={null} cameraButton={false} activeQuestion={activeQuestion} />
+              </div>
+            </>
           )}
-          <Record />
         </div>
-    
       </div>
     </div>
   );
