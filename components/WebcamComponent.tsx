bcf9570ad9ebdbe7ba8c54308378b3dfa6961116
Changwhi <whinada@gmail.com>
2024-07-01 01:29:15 -0700

    feat: create webcam function and save function to store answers in db

--- components/WebcamComponent.tsx
+++ components/WebcamComponent.tsx
@@ -1,16 +1,34 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Lightbulb, Mic, WebcamIcon } from "lucide-react";
+import { Circle, Lightbulb, LoaderCircle, Mic, WebcamIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Webcam from "react-webcam";
 import useSpeechToText from "react-hook-speech-to-text";
+import { toast } from "sonner";
+import { MockInterviewType } from "@/type/interviewType";
+import { chatSession } from "@/utils/GeminiAi";
+import { db } from "@/utils/db";
+import { Answer } from "@/utils/schema";
+import moment from "moment";
+import { serial } from "drizzle-orm/pg-core";
+import { useUser } from "@clerk/nextjs";
 
 const videoConstraints = {
   width: 1280,
   height: 720,
   facingMode: "user",
 };
-function WebcamComponent({ cameraButton }: { cameraButton: boolean }) {
+function WebcamComponent({
+  cameraButton,
+  questions,
+  activeQuestion,
+  interviewInfo,
+}: {
+  cameraButton: boolean;
+  questions: any;
+  activeQuestion: number;
+  interviewInfo: any;
+}) {
   const [userAnswer, setUserAnswer] = useState("");
   const {
     error,
@@ -29,74 +47,124 @@ function WebcamComponent({ cameraButton }: { cameraButton: boolean }) {
       setUserAnswer((prevAns) => prevAns + result?.transcript);
     });
   }, [results]);
+  const { user } = useUser();
 
   const [webCamEnabled, setWebCamEnabled] = useState(false);
-  return webCamEnabled ? (
+  const [loading, setLoading] = useState(false);
+
+  const saveAnswer = async () => {
+    if (isRecording) {
+      setLoading(true);
+      stopSpeechToText();
+      if (userAnswer.length < 10) {
+        setLoading(false);
+        toast("Please speak at least 10 words");
+        return;
+      } else if (questions) {
+        const feedbackPrompt =
+          "Qeustion: " +
+          questions[activeQuestion]?.question +
+          " Answer: " +
+          userAnswer +
+          ",depends on question and user answer for given interview question" +
+          " please give us rating for answer and feedback as area of improvement if any" +
+          "in just 3 to 5 lines to improve it in JSON format with rating field and feedback field";
+        const result = await chatSession.sendMessage(feedbackPrompt);
+        const mockJsonResp = result.response
+          .text()
+          .replace("```json", "")
+          .replace(/^\s+/, "")
+          .replace("```", "");
+        console.log(mockJsonResp);
+        const responseData = JSON.parse(mockJsonResp);
+
+        const respDB = await db.insert(Answer).values({
+          mockIdRef: interviewInfo?.mockId as string,
+          question: questions[activeQuestion]?.question as string,
+          userAnswer: userAnswer as string,
+          feedback: responseData?.feedback as string,
+          rating: responseData?.rating as string,
+          createdAt: moment().format("DD-MM-yyyy") as string,
+          correctAns: questions[activeQuestion]?.answer as string,
+          userEmail: user?.primaryEmailAddress?.emailAddress as string,
+        });
+        if (respDB) {
+          toast("Answer saved successfully");
+        }
+        setUserAnswer("");
+        setLoading(false);
+      } else {
+        toast("No questions found");
+      }
+    } else {
+      startSpeechToText();
+    }
+  };
+  
+  
+  return (
     <>
-      <Webcam
-        style={{
-          height: 300,
-          width: "100%",
-          zIndex: 10,
-        }}
-        mirrored={true}
-        videoConstraints={videoConstraints}
-      />
-      {cameraButton && (
-        <Button
-          variant={"outline"}
-          className="self-center my-2"
-          onClick={() => setWebCamEnabled(false)}
-        >
-          Disable Webcam
-        </Button>
+      {loading && (
+        <div className="flex justify-center items-center">
+          <LoaderCircle className="w-10 h-10 animate-spin" />
+        </div>
       )}
-      {!cameraButton && (
-        <Button
-          className="my-2"
-          variant={"outline"}
-          onClick={isRecording ? stopSpeechToText : startSpeechToText}
-        >
-          {isRecording ? (
-            <h2>
-              <Mic /> Recording...
-            </h2>
-          ) : (
-            "Record"
+      {!loading && webCamEnabled && (
+        <>
+          <Webcam
+            style={{
+              height: 300,
+              width: "100%",
+              zIndex: 10,
+            }}
+            mirrored={true}
+            videoConstraints={videoConstraints}
+          />
+          {cameraButton && (
+            <Button
+              variant={"outline"}
+              className="self-center my-2"
+              onClick={() => setWebCamEnabled(false)}
+            >
+              Disable Webcam
+            </Button>
           )}
-        </Button>
-      )}
-    </>
-  ) : (
-    <>
-      <div className="h-full w-full items-center flex flex-col justify-center text-center text-semibold border bg-secondary rounded-md">
-        <WebcamIcon className="w-20 h-20" />
-      </div>
-      {cameraButton && (
-        <Button
-          variant={"outline"}
-          className="self-center my-2"
-          onClick={() => setWebCamEnabled(true)}
-        >
-          Enable Webcam
-        </Button>
-      )}
-      {!cameraButton && (
-        <Button
-          className="my-2"
-          variant={isRecording ? "destructive" : "outline"}
-          onClick={isRecording ? stopSpeechToText : startSpeechToText}
-        >
-          {isRecording ? (
-               "Stop"
-          ) : (
-            "Record"
+          {!cameraButton && (
+            <Button className="my-2" variant={"outline"} onClick={saveAnswer}>
+              {isRecording ? "Stop" : "Record"}
+            </Button>
           )}
-        </Button>
+        </>
       )}
-      <Button onClick={() => console.log(userAnswer)}>Show user answer</Button>
+      {!loading && !webCamEnabled && (
+        <>
+          <div className="h-full w-full items-center flex flex-col justify-center text-center text-semibold border bg-secondary rounded-md">
+            <WebcamIcon className="w-20 h-20" />
+          </div>
+          {cameraButton && (
+            <Button
+              variant={"outline"}
+              className="self-center my-2"
+              onClick={() => setWebCamEnabled(true)}
+            >
+              Enable Webcam
+            </Button>
+          )}
+          {!cameraButton && (
+            <Button
+              className="my-2"
+              variant={isRecording ? "destructive" : "outline"}
+              onClick={saveAnswer}
+            >
+              {isRecording ? "Stop" : "Record"}
+            </Button>
+          )}
+          <Button onClick={() => console.log(userAnswer)}>
+            Show user answer
+          </Button>
+        </>
+      )}
     </>
   );
 }
-
 export default WebcamComponent;
