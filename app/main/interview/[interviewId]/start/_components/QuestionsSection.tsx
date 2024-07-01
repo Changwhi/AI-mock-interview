27c96ec2eacd6ad0fde9243f17cfae298b250a1f
Changwhi <whinada@gmail.com>
2024-07-01 01:04:59 -0700

    feat: implement function to read questions aloud using voice

--- app/main/interview/[interviewId]/start/_components/QuestionsSection.tsx
+++ app/main/interview/[interviewId]/start/_components/QuestionsSection.tsx
@@ -1,34 +1,55 @@
 import { INFORMATION_ABOUT_THE_QUESTION } from "@/text/interview";
-import { Lightbulb } from "lucide-react";
+import { Lightbulb, Volume2 } from "lucide-react";
 import React from "react";
 
-function QuestionsSection({ questions, activeQuestion }: any) {
-  return  questions && (
-    <div className="p-5 border rounded-lg">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5 my-10">
-        {questions &&
-          questions.map((question: any, index: number) => (
-            <h2
-              key={index}
-              className={`p-2 rounded-full text-xs md:text-sm text-center cursor-pointer ${
-                activeQuestion == index ? "bg-primary text-white" : "bg-white"
-              }`}
-            >
-              {" "}
-              Question #{index + 1}
-            </h2>
-          ))}
+function QuestionsSection({
+  questions,
+  activeQuestion,
+  setActiveQuestion,
+}: any) {
+    const textToSpeach = (text: string) => {
+        if ('speechSynthesis' in window) {
+            const speech = new SpeechSynthesisUtterance();
+            speech.text = text;
+            speech.lang = 'en-US';
+            speech.pitch = 1;
+            speech.volume = 1;
+            speech.rate = 1;
+            speechSynthesis.speak(speech);
+        }else{
+            alert("Your browser does not support text to speech");
+        }
+    }
+  return (
+    questions && (
+      <div className="p-5 border rounded-lg">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 my-5">
+          {questions &&
+            questions.map((_: any, index: number) => (
+              <h2
+                onClick={() => setActiveQuestion(index)}
+                key={index}
+                className={`p-2 rounded-full text-xs md:text-sm text-center cursor-pointer ${
+                    activeQuestion == index ? "bg-primary text-white" : "bg-secondary"
+                }`}
+                >
+                Qeustion #{index + 1}
+              </h2>
+            ))}
+        </div>
+        <h2 className="md:h-40 my-5 text-sm md:text-base">
+          {questions[activeQuestion]?.question}
+            <Volume2 className="cursor-pointer" onClick={() => textToSpeach(questions[activeQuestion]?.question)} />
+        </h2>
+        <div className="border rounded-lg p-5 bg-blue-100 mt-10">
+          <h2 className="flex gap-2 items-center text-primary">
+            <Lightbulb />
+            <strong>Information</strong>
+          </h2>
+          <h2>{INFORMATION_ABOUT_THE_QUESTION}</h2>
+        </div>
       </div>
-          <h2 className="my-5 text-sm md:text-lg">{questions[activeQuestion]?.question}</h2>
-          <div className="border rounded-lg p-5 bg-blue-100 mt-24">
-            <h2 className="flex gap-2 items-center text-primary">
-                <Lightbulb />
-                <strong>Information</strong>
-            </h2>
-            <h2 >{INFORMATION_ABOUT_THE_QUESTION}</h2>
-          </div>
-    </div>
-
+    )
   );
 }
 
