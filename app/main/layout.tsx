ded50f924a08248fb5b853d9ca63dfa8f7b55c1c
Changwhi <whinada@gmail.com>
2024-07-02 15:15:39 -0700

    design: move Header component to root layout

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import Header from "@/components/main/Header";
 export default function MainLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <section>
-      <Header />
-      {/* Include shared UI here e.g. a header or sidebar */}
-      <div className="mx-5 md:mx-20 lg:mx-36">{children}</div>
+      <div className="mx-5 md:mx-20 lg:mx-36 flex justify-center">{children}</div>
     </section>
   );
 }
