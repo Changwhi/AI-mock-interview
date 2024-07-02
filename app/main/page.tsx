752dd45d45519af7591beaa0c329a46649a564db
Changwhi <whinada@gmail.com>
2024-07-01 20:54:57 -0700

    feat: import interviewRecords component

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,19 +1,19 @@
-import AddNewInterview from '@/components/main/AddNewInterview'
-import { UserButton } from '@clerk/nextjs'
-import React, { useEffect } from'react'
+import AddNewInterview from "@/components/main/AddNewInterview";
+import InterviewRecords from "@/components/main/InterviewRecords";
+import { UserButton } from "@clerk/nextjs";
+import React, { useEffect } from "react";
 
 const Main = () => {
-
   return (
-    <div className='p-10'>
-      <h2 className='font-bold text-2xl'>Main</h2>
-      <h2 className='text-gray-500'>Start your Mockup Interview with AI!</h2>
-      <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
+    <div className="p-10">
+      <h2 className="font-bold text-2xl">Main</h2>
+      <h2 className="text-gray-500">Start your Mockup Interview with AI!</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 my-5">
         <AddNewInterview />
       </div>
+      <InterviewRecords />
     </div>
+  );
+};
 
-  )
-}
-
-export default Main
+export default Main;
