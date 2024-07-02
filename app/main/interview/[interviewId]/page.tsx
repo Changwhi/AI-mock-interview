13a3ac4d1d7ecfc8522f5e851465a7203a4806f2
Changwhi <whinada@gmail.com>
2024-07-02 15:17:04 -0700

    design: adjust width and import image

--- app/main/interview/[interviewId]/page.tsx
+++ app/main/interview/[interviewId]/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import React from "react";
-import { Lightbulb, WebcamIcon } from "lucide-react";
-import { INFORMATION_ABOUT_THE_INTERVIEW } from "@/text/interview";
+import { Lightbulb} from "lucide-react";
+import { INFORMATION_ABOUT_THE_INTERVIEW } from "@/text/RegularText";
 import Link from "next/link";
 import useGetInterviewDetails from "@/lib/useGetInterviewDetails";
-import WebcamComponent from "../../../../components/WebcamComponent";
 import { Skeleton } from "@/components/ui/skeleton";
+import Image from "next/image";
+
 
 function Interview({ params }: { params: { interviewId: string } }) {
   const interviewInfo = useGetInterviewDetails(params.interviewId);
@@ -35,45 +36,51 @@ function Interview({ params }: { params: { interviewId: string } }) {
   }
 
   return (
-    <div className="p-10 flex flex-col">
-      <h2 className="font-bold text-2xl">Ready to Mockup Interview</h2>
-      <h2 className="text-gray-500">Check your Mockup Interview details</h2>
-      <div className="flex justify-center items-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-          <div className="grid grid-cols-1 my-5 gap-5">
-            <div className="flex flex-col p-5 rounded-lg border gap-5">
-              <h2 className="text-lg">
-                <strong>Job Position: </strong>
-                {interviewInfo?.jobPosition}
-              </h2>
-              <h2 className="text-lg">
-                <strong>Job Description: </strong>
-                {interviewInfo?.jobDescription}
-              </h2>
-              <h2 className="text-lg">
-                <strong>Job Experience: </strong>
-                {interviewInfo?.jobExperience} year(s)
-              </h2>
+      <div className="p-10 flex flex-col lg:w-3/4">
+        <h2 className="font-bold text-2xl">Ready to Mockup Interview</h2>
+        <h2 className="text-gray-500">Check your Mockup Interview details</h2>
+        <div className="flex justify-center items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+            <div className="grid grid-cols-1 my-5 gap-5">
+              <div className="flex flex-col p-5 rounded-lg border gap-5">
+                <h2 className="text-lg">
+                  <strong>Job Position: </strong>
+                  {interviewInfo?.jobPosition}
+                </h2>
+                <h2 className="text-lg">
+                  <strong>Job Description: </strong>
+                  {interviewInfo?.jobDescription}
+                </h2>
+                <h2 className="text-lg">
+                  <strong>Job Experience: </strong>
+                  {interviewInfo?.jobExperience} year(s)
+                </h2>
+              </div>
+              <div className="flex flex-col p-5 rounded-lg text-yellow-700 border gap-5 bg-yellow-100">
+                <h2 className="text-lg flex items-center ">
+                  {" "}
+                  <Lightbulb className=" h-7 w-7 mb-3" />
+                  <strong>Information</strong>
+                </h2>
+                <h2>{INFORMATION_ABOUT_THE_INTERVIEW}</h2>
+              </div>
             </div>
-            <div className="flex flex-col p-5 rounded-lg text-yellow-700 border gap-5 bg-yellow-100">
-              <h2 className="text-lg flex items-center ">
-                {" "}
-                <Lightbulb className=" h-7 w-7 mb-3" />
-                <strong>Information</strong>
-              </h2>
-              <h2>{INFORMATION_ABOUT_THE_INTERVIEW}</h2>
+            <div className="flex flex-col justify-center">
+            <Image 
+              src={"/interview.png"}
+              alt="Interview Image"
+              width={500}
+              height={500}
+              className="rounded-xl"
+            >
+              </Image> 
             </div>
-          </div>
-          <div className="flex flex-col justify-center gap-4">
-            {/* <Webcam /> */}
-            <WebcamComponent interviewInfo={null} activeQuestion={0} questions={null} cameraButton={true} />
           </div>
         </div>
+        <Link href={`/main/interview/${params.interviewId}/start`}>
+          <Button className="self-center">Let&#39;s start</Button>
+        </Link>
       </div>
-      <Link href={`/main/interview/${params.interviewId}/start`}>
-        <Button className="self-center">Let&#39;s start</Button>
-      </Link>
-    </div>
   );
 }
 
