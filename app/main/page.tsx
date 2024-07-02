bccc2ba2f51042271718bc50d07debf3a1a8b0e3
Changwhi <whinada@gmail.com>
2024-07-02 15:16:04 -0700

    design: adjust width when screen size becomes large

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,11 +1,10 @@
 import AddNewInterview from "@/components/main/AddNewInterview";
 import InterviewRecords from "@/components/main/InterviewRecords";
-import { UserButton } from "@clerk/nextjs";
-import React, { useEffect } from "react";
+import React from "react";
 
 const Main = () => {
   return (
-    <div className="p-10">
+    <div className="p-10 lg:w-3/4">
       <h2 className="font-bold text-2xl">Main</h2>
       <h2 className="text-gray-500">Start your Mockup Interview with AI!</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 my-5">
