f6d982519943cc2f98bc44923dddf0e563cab0d7
Changwhi <whinada@gmail.com>
2024-07-01 19:22:41 -0700

    feat: implement interface for answer's type

---
+++ type/answerType.tsx
@@ -0,0 +1,11 @@
+export interface AnswerType {
+    id: number;
+    mockIdRef: string;
+    question: string;
+    correctAns: string;
+    userAnswer: string;
+    feedback: string;
+    userEmail: string;
+    rating: string;
+    createdAt: string;
+}
