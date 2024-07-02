2122035d3f7bd8a76141beb86b5fe527096f8a44
Changwhi <whinada@gmail.com>
2024-07-02 14:56:11 -0700

    design: add landing component to root route

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
+import Landing from "@/components/landing/Landing";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant={"outline"}> Hello </Button>
-    </main>
+     <Landing />
   );
 }
